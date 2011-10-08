2aef51638c6643b8dcefe0e84765d8dc4ee52093
ron bolzern <ron@dominosite.com>
2011-10-08 15:31:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9b93165f767544ae403e0e435b657bd23c70f84ae44897d8025dd328a53d36d7b672fa814659de72a0c718ec4aa01228fed87d9abfc5d9b67cc37b73ee426011'
